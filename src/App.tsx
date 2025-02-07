75c4f7d2bda2cb5ce8f7348a3221f9032a586ae6
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2025-02-07 17:21:26 -0300

    set connect only

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
         environmentId: "83bee83b-81e0-44c3-843c-435ab186f470",
         apiBaseUrl: "https://app.dynamic-preprod.xyz/api/v0",
         walletConnectors: [EthereumWalletConnectors],
+        initialAuthenticationMode: "connect-only",
       }}
     >
       <DynamicWidget />
